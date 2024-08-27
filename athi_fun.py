e04201d565cd3e0b5386090b16fe9be3d1fba535
DEEPAK MANDAPALLI <152033387+MDeepak23@users.noreply.github.com>
2024-08-27 11:08:46 +0530

    Add files via upload

---
+++ athi_fun.py
@@ -0,0 +1,17 @@
+def add(a,b):
+	return a+b
+def sub(a,b):
+	return a-b
+def multi(a,b):
+	return a*b
+def divi(a,b):
+	return a//b
+
+a = int(input("enter a:"))
+b = int(input("enter b:"))
+print(add(a,b))
+print(sub(a,b))
+print(multi(a,b))
+print(divi(a,b))
+
+
